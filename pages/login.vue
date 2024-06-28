8d2eab8aeeb2210a50fd1f21370a5067f2cd37e3
Wilgas ADJOVI <Wilfrieda216@gmail.com>
2024-06-28 17:16:59 +0200

    register

--- pages/login.vue
+++ pages/login.vue
@@ -1,6 +1,19 @@
 <template>
-  <div>
-    <p>Login</p>
+  <div class="prose m_form bg-white p-6 rounded-lg shadow-lg mt-10">
+    <h1>Connexion</h1> 
+    <FormKit type="form" submit-label="Se connecter"  default="{value}" class="m_form">
+      <FormKit
+      type="email" label="Email" nema="email"  placeholder="Votre adresse email"
+      />
+      <FormKit
+      type="password" label="mot de passe" name="password" placeholder="Votre mot de passe"
+      />
+    </FormKit>
+
+    <p class="text-red-600">
+      Pas de compte ?
+      <NuxtLink class="uderline text-lime-600" to="/login">Inscrivez-Vous !</NuxtLink>
+    </p>
   </div>
 </template>
 
@@ -8,6 +21,8 @@
 
 </script>
 
-<style>
-
+<style >
+.m_form{
+  margin-inline: auto;
+}
 </style>
--- pages/register.vue
+++ pages/register.vue
@@ -1,9 +1,52 @@
 <template>
-  <div>
+
+  <div class="prose">
+    <h1>Inscription</h1>
+
+
+    <FormKit 
+    type="form" 
+    submit-label="s'inscrire" 
+    :submit-attrs="{inputClass: 'btn'}" 
+    #default="{value}" 
+    @submit="">
+    <FormKit 
+    name="name" 
+    label="Nom et prenom" 
+    type="text" 
+    placeholder="Wilgas HOUNFONADAN"/>
+    <FormKit 
+    name="email" 
+    label="Email" 
+    type="email"  
+    validation="required|email" 
+    placeholder="jdoe@exemple.com"/>
+    <FormKit 
+    name="password" 
+    label="Votre Mot de Passe" 
+    type="password" 
+    validation="required"  
+    placeholder="Votre mot de passe"/>
+    <FormKit 
+    name="password_confirmation" 
+    label="Confirmation mot de passe" 
+    type="password" 
+    validation="required|confirm:password"  
+    placeholder="Confirmez votre mot de passe"/>
+    </FormKit>
+
+    <p>
+      Vous avez déjà un compte ?
+      <NuxtLink class="uderline text-lime-600" to="/login">connectez-vous!</NuxtLink>
+    </p>
   </div>
 </template>
 
 <script lang="ts" setup>
+
+
+
+ 
 
 </script>
 
