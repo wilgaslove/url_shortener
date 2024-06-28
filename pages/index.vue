8577254be100e41ecffb04c4b06ee88eeebf4eb8
Wilgas ADJOVI <Wilfrieda216@gmail.com>
2024-06-28 15:10:40 +0200

    login&register

--- pages/index.vue
+++ pages/index.vue
@@ -1,14 +1,19 @@
 <template>
-  <header>
-    <nav class="">
-      <NuxtLink to="#">URLShortify</NuxtLink>
-
-      <div class="espace-x-3 bg-black text-white">
-        <NuxtLink to="#">Se connecter</NuxtLink>
-        <NuxtLink to="#">S'inscrire</NuxtLink>
+  <div class="bg-slate-100 h-full w-full">
+    <header class="relative ">
+      <nav class="flex justify-between fixed top-0 left-0 right-0 bg-black text-white font-bold px-[3%] py-2 h-[5%] ">
+        <NuxtLink to="#">URLShortify</NuxtLink>
+        <div class=" ">
+          <NuxtLink to="/login" class="mx-4">Se connecter</NuxtLink>
+          <NuxtLink to="/register">S'inscrire</NuxtLink>
+        </div>
+      </nav>
+    </header>
+      <div class="text-black text-center pt-[20%]">
+        <p>Hello Nuxt! 🚀👩‍🚀</p>
       </div>
-    </nav>
-  </header>
+    
+  </div>
 </template>
 
 <script lang="ts" setup>
---
+++ pages/login.vue
@@ -0,0 +1,13 @@
+<template>
+  <div>
+    <p>Login</p>
+  </div>
+</template>
+
+<script lang="ts" setup>
+
+</script>
+
+<style>
+
+</style>
