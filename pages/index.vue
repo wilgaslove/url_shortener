80bb1340596c66379f8d37f54e31fe6ed24e3d12
Wilgas ADJOVI <Wilfrieda216@gmail.com>
2024-06-28 14:22:14 +0200

    initialisation du projet

---
+++ pages/index.vue
@@ -0,0 +1,20 @@
+<template>
+  <header>
+    <nav class="">
+      <NuxtLink to="#">URLShortify</NuxtLink>
+
+      <div class="espace-x-3">
+        <NuxtLink to="#">Se connecter</NuxtLink>
+        <NuxtLink to="#">S'inscrire</NuxtLink>
+      </div>
+    </nav>
+  </header>
+</template>
+
+<script lang="ts" setup>
+
+</script>
+
+<style>
+
+</style>
